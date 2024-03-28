3e5d69d6c202719c28079453e618ce6f09837d74
Rossana Ventrella <rossana.ventrella@checkatrade.com>
2024-03-28 11:33:37 +0000

    adding nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // src/App.js
 import React from 'react';
 import GoogleCalendar from './components/GoogleCalendar';
+import NavBar from './components/NavBar';
 
 function App() {
     return (
         <div>
             <h1>My Google Calendar App</h1>
             <GoogleCalendar />
+            <NavBar />
         </div>
     );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>
+      <img src="images/logo.jpg" alt="Dream N I Logo" />
+        <h1>Dream and I</h1>
+        <nav>
+            <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#initiatives">Initiatives</a></li>
+                <li><a href="#resources">Resources</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
